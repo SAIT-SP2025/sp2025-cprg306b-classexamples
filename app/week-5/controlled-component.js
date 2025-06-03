9592163ba506388e73cc8f181c40e766848fdde7
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2025-06-03 13:17:37 -0600

    Week 5 Exmaples part 2

--- app/week-5/controlled-component.js
+++ app/week-5/controlled-component.js
@@ -4,52 +4,92 @@ import { useState } from "react";
 
 export default function RegistrationForm() {
 
+    // Form State variables
     const [fullName, setFullName] = useState("");
-    const [displayName, setDisplayName] = useState(false);
+    const [email, setEmail] = useState("");
+    const [birthDate, setBirthDate] = useState("");
+    const [subscription, setSubscription] = useState("");
+    const [message, setMessage] = useState("");
 
-
-
+    // form change handle functions
     const handleFullNameChange = (event) => {
         console.dir(event.target.value);
         setFullName(event.target.value);
     }
+    const handleEmailChange = (event) => setEmail(event.target.value);
+    const handleBirthDateChange = (event) => setBirthDate(event.target.value);
+    const handleSubscriptionChange = (event) => setSubscription(event.target.value);
+    const handleMessageChange = (event) => setMessage(event.target.value);
 
+    // form submission function
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        if(subscription == ""){
+            alert("please select a plan");
+            return;
+        }
+
+        let registrationObject = {
+            fName: fullName,
+            mail: email,
+            bDay: birthDate,
+            plan: subscription,
+            msg: message
+        }
+
+        alert(`
+            Name: ${registrationObject.fName} | 
+            Email: ${registrationObject.mail} |
+            Birthday: ${registrationObject.bDay} |
+            Plan Type: ${registrationObject.plan} |
+            Message: ${registrationObject.msg}
+        `);
+
+        setFullName("");
+        setEmail("");
+        setBirthDate("");
+        setSubscription("");
+        setMessage("");
+
     }
 
-    const handleDisplayName = () => setDisplayName(fullName);
-
     return(
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Full Name:</label>
-                <input type="text" className="border" onChange={handleFullNameChange} onBlur={handleDisplayName}  />
-                {displayName && <p>Welcome, {displayName}</p>}
+        <form onSubmit={handleSubmit} className="p-4 bg-blue-100">
+            <div className="mb-3">
+                <label className="inline-block w-40">Full Name:</label>
+                <input
+                    type="text"
+                    className="border border-blue-500 bg-white px-2 py-1 rounded focus:bg-amber-100"
+                    onChange={handleFullNameChange}
+                    value={fullName}
+                    required={true}
+                />
             </div>
-            <div>
-                <label>Email:</label>
-                <input type="email" className="border" name="user_mail" />
+            <div className="mb-3">
+                <label className="inline-block w-40">Email:</label>
+                <input type="email" required={true} className="border border-blue-500 bg-white px-2 py-1 rounded focus:bg-amber-100" name="user_mail" onChange={handleEmailChange} value={email} />
             </div>
-            <div>
-                <label>Birth Date:</label>
-                <input type="date" className="border" />
+            <div className="mb-3">
+                <label className="inline-block w-40">Birth Date:</label>
+                <input type="date" className="border border-blue-500 bg-white px-2 py-1 rounded focus:bg-amber-100" onChange={handleBirthDateChange} value={birthDate} />
             </div>
-            <div>
-                <label>Subscription Plan:</label>
-                <select className="border">
-                    <option>Free Plan</option>
-                    <option>Basic Plan</option>
-                    <option>Premium Plan</option>
-                </select>
+            <div className="mb-3">
+                <label className="inline-block w-40">Subscription Plan:</label>
+                <select className="border border-blue-500 bg-white px-2 py-1 rounded focus:bg-amber-100" onChange={handleSubscriptionChange} value={subscription}>
+                    <option disabled value="">--- Please Select a Plan ---</option>
+                    <option value="free">Free Plan</option>
+                    <option value="basic">Basic Plan</option>
+                    <option value="premium">Premium Plan</option>
+                </select> 
             </div>
-            <div>
-                <label className="align-top">Message:</label>
-                <textarea className="border"></textarea>
+            <div className="mb-3">
+                <label className="inline-block w-40 align-top">Message:</label>
+                <textarea className="border border-blue-500 bg-white px-2 py-1 rounded focus:bg-amber-100" onChange={handleMessageChange} value={message}></textarea>
             </div>
 
-            <div>
-                <button>Submit Registration</button>
+            <div className="mb-3">
+                <button className="bg-blue-500 text-white rounded px-3 py-2 hover:bg-green-500 active:bg-amber-500">Submit Registration</button>
             </div>
         </form>
     );
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,13 +1,31 @@
+"use client"
 
-
+import { useState } from "react";
 import RegistrationForm from "./controlled-component";
 
 export default function RegistrationPage(){
 
+    const [formOpen, setFormOpen] = useState(false);
+
+    const toggleForm = () => {
+        if ( formOpen ){
+            setFormOpen(false);
+        } else {
+            setFormOpen(true);
+        }
+    }
+
+    const toggleForm2 = () => setFormOpen(!formOpen);
+
     return(
         <main>
-            <h1>Registration</h1>
-            <RegistrationForm />
+            <h1 className="text-3xl text-center my-4 font-serif font-black">Registration</h1>
+            <button onClick={toggleForm2}>Toggle Registration Form</button>
+
+            { formOpen && <RegistrationForm /> }
+
+            {/* <p>{formOpen ? "Hello!" : "Something Else"}</p> */}
+            {/* <RegistrationForm /> */}
         </main>
     );
 }
